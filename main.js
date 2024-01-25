6782da5867082368c159bac13176713e7b7091b5
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2024-01-25 23:14:26 +0530

    Js code is reduced and also the documentation of the some codes is added

--- main.js
+++ main.js
@@ -1,39 +1,55 @@
-var randomNumber = Math.floor(Math.random()*100)+1;
+// random number generator
+var randomNumber = Math.floor(Math.random() * 100) + 1;
 
-
+// accessing html elements
 var num = document.getElementById("val");
 
 var startButton = document.getElementById("submit");
 
-var scoreBoard = document.getElementById("score"); 
+var scoreBoard = document.getElementById("score");
 
 var mistakeStatus = document.getElementById("mistake");
 
-var runStatus =document.getElementById("run");
+var runStatus = document.getElementById("run");
 
 var resetButton = document.getElementById("reset");
 
+// storing guessing values
+var guessAraary = []
 
-var guessAraary=[]
+// it's a life time 
+var lifeTime = 10;
 
-mistakeStatus.style.color= "red";
-mistakeStatus.style.fontWeight ='bolder';
-var  lifeTime=10;
+var win = false;
 
-var win=false;
-
-
-function rainbowGif(){
+//  ribbon falling ribbon 
+function rainbowGif() {
     let img = document.createElement("img")
-
-  img.setAttribute("src","./rainbow.gif")
-  document.body.appendChild(img)
-
+    img.setAttribute("src", "./rainbow.gif")
+    document.body.appendChild(img)
 }
-startButton.addEventListener("click",begin)
+// mistake status function
+function  mistakes(value,check) {
+    if (check) {
+        mistakeStatus.style.color = "red";
+    }else{
+        mistakeStatus.style.color = "green";
+    }
+    mistakeStatus.style.fontWeight = 'bolder';
+    mistakeStatus.textContent = value;
+ 
+}
 
+// runstatus function
+function runs(value) {
+    runStatus.textContent = value;
+}
+
+startButton.addEventListener("click", begin)
+
+
+//cheacking lifetime before starting game
 function begin() {
-    
     if (lifeTime != 0) {
         startGame()
     } else {
@@ -41,51 +57,49 @@ function begin() {
     }
 }
 
+// starting game function
 function startGame() {
     let guessNumber = num.value;
-        if (guessNumber > randomNumber) {
-            guessAraary.push(guessNumber)
-            lifeTime--;
-           
-            scoreBoard.textContent = `Previous ${guessAraary.join(" ")}`;
-            mistakeStatus.textContent="Wrong!"
-            runStatus.textContent ="Last guess was too high!";
-        } else if (guessNumber < randomNumber) {
-            guessAraary.push(guessNumber);
-            lifeTime--;  
-            scoreBoard.textContent = `Previous: ${guessAraary.join(" ")}`;
-            mistakeStatus.textContent="Wrong!"
-            runStatus.textContent ="Last guess was too low!";
-        } else{
-            guessAraary.push(guessNumber);
-            win=true;
-            endGame();
-
-        }
-    } 
-    
-
-function endGame(params) {
-     if (win == true) {
-        mistakeStatus.style.color="green";
-        scoreBoard.textContent = `Number of guesses: ${10-lifeTime}`;
-        mistakeStatus.textContent="Your Win!";
-        runStatus.textContent =null;
-        rainbowGif();
-        startButton.removeEventListener("click",begin)
-        startButton.disabled = true;
-        
-        
-     } else {
+    if (guessNumber > randomNumber) {
+        guessAraary.push(guessNumber)
+        lifeTime--;
+        scoreBoard.textContent = `Previous ${guessAraary.join(" ")}`;
+        mistakes("Wrong!",true);
+        runs("Last guess was too high!");
+    } else if (guessNumber < randomNumber) {
+        guessAraary.push(guessNumber);
+        lifeTime--;
         scoreBoard.textContent = `Previous: ${guessAraary.join(" ")}`;
-        mistakeStatus.textContent="Your loss!"
-        runStatus.textContent="Your chances is finished"
-        startButton.removeEventListener("click",begin)
-        startButton.disabled = true;
-     }
+        mistakes("Wrong!",true);
+        runs("Last guess was too low!");
+    } else {
+        guessAraary.push(guessNumber);
+        win = true;
+        endGame();
+    }
 }
 
+// Ending game function
+function endGame(params) {
+    if (win == true) {
+        scoreBoard.textContent = `Number of guesses: ${10 - lifeTime}`;
+        mistakes("Your Win!",false);
+        runs(null);
+        rainbowGif();
+        startButton.removeEventListener("click", begin)
+        startButton.disabled = true;
 
-resetButton.addEventListener("click",()=>{
-     location.reload()
+
+    } else {
+        scoreBoard.textContent = `Previous: ${guessAraary.join(" ")}`;
+        mistakes("Your loss!",true);
+        runs("Your chances is finished");
+        startButton.removeEventListener("click", begin)
+        startButton.disabled = true;
+    }
+}
+
+// starting new game button
+resetButton.addEventListener("click", () => {
+    location.reload()
 })
